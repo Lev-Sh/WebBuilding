244efd48c663da14b6fcee736feed50c65be5a49
Lev-Sh <65592917+lev-sh@users.noreply.github.com>
2024-02-20 20:10:19 +0300

    --WEBSITE

--- server.py
+++ server.py
@@ -7,5 +7,17 @@ app = Flask(__name__)
 @app.route('/index/<title>')
 def index(title):
     return  render_template('base.html', title=title)
+@app.route('/list_prof/<type_list>')
+def list_prof(type_list):
+    result = []
+    with open('profesions.txt', 'w' ,encoding='utf-8') as f:
+        result = list(map(lambda x: x.rstrip(), f.read().split('*')))
+    params = {
+        'proffessions': result,
+        'title': 'Список проффесий',
+        'type_list': type_list
+    }
+    return render_template('list_prof.html', **params)
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
