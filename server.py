bb2f166bdc8b304685d2decb9a5b08dddf4d80eb
Lev-Sh <65592917+lev-sh@users.noreply.github.com>
2024-02-25 12:33:50 +0300

    --WEBSITE

--- server.py
+++ server.py
@@ -10,14 +10,14 @@ def index(title):
 @app.route('/list_prof/<type_list>')
 def list_prof(type_list):
     result = []
-    with open('profesions.txt', 'w' ,encoding='utf-8') as f:
+    with open('profesions.txt', 'r',encoding='utf-8') as f:
         result = list(map(lambda x: x.rstrip(), f.read().split('*')))
-    params = {
-        'proffessions': result,
-        'title': 'Список проффесий',
-        'type_list': type_list
-    }
-    return render_template('list_prof.html', **params)
+        params = {
+            'proffessions': result,
+            'title': 'Список проффесий',
+            'type_list': type_list
+        }
+        return render_template('list_prof.html', **params)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
